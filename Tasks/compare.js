0c47d03731cd555a06b25d3fef7315d878584873
scvotr <71510141+scvotr@users.noreply.github.com>
2023-09-30 14:25:04 +0300

    changed name variable

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -3,8 +3,8 @@
 let compare = (first_values, ...parameters_list) => {
   const second_values = parameters_list[0];
   let firstValuesKey = Object.keys(first_values);
-  let b = Object.keys(second_values);
-  if (firstValuesKey.join('-') !== b.join('-')) return false;
+  let secondValuesKeys = Object.keys(second_values);
+  if (firstValuesKey.join('-') !== secondValuesKeys.join('-')) return false;
   let e = true;
   for (c of firstValuesKey) {
     if (first_values[c] === second_values[c]) e = e && true;
